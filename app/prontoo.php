bb190b957f46957331f0d5fc09086e504503e6c4
Wesley Ferreira <48494254+drwesleyadv@users.noreply.github.com>
2026-07-17 09:28:41 -0400

    Atualiza constantes da versão 1.7.17.1

--- app/prontoo.php
+++ app/prontoo.php
@@ -42,7 +42,7 @@ if (!function_exists("h")) {
         );
     }
 }
-const PRONTOO_VERSION_FALLBACK = "1.7.15.10";
+const PRONTOO_VERSION_FALLBACK = "1.7.17.1";
 const PRONTOO_ASSET_REV_FALLBACK = "1.7.15.10";
 function prontoo_release_metadata(): array
 {
@@ -153,8 +153,8 @@ function prontoo_version_contract_status(): array
     ];
     return $status;
 }
-const PRONTOO_PREVIOUS_VERSION = "1.7.15.2";
-const PRONTOO_PREVIOUS_ASSET_REV = "1.7.15.2";
+const PRONTOO_PREVIOUS_VERSION = "1.7.15.10";
+const PRONTOO_PREVIOUS_ASSET_REV = "1.7.15.10";
 unset($prontooReleaseMetadata, $prontooVersion, $prontooRelease, $prontooAssetRevision);
 const PRONTOO_MIN_PHP_VERSION = "8.4.0";
 const PRONTOO_MIN_MYSQL_VERSION = "8.0.30";
