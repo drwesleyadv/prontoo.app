756907e1c79fc3850b069ad3b483a7fc8d356645
Wesley Ferreira <48494254+drwesleyadv@users.noreply.github.com>
2026-07-17 09:48:19 -0400

    Sincroniza landing com a versão 1.7.17.2

--- br/index.php
+++ br/index.php
@@ -1,7 +1,7 @@
 <?php
 
 const BR_LANDING_ORIGIN = "https://prontoo.app";
-const BR_LANDING_VERSION_FALLBACK = "1.7.17.1";
+const BR_LANDING_VERSION_FALLBACK = "1.7.17.2";
 function br_landing_release_metadata(): array
 {
     static $metadata = null;
