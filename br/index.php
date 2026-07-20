a9c7ff47624db0661cf27ccd84094087d7d330ca
Wesley Ferreira <48494254+drwesleyadv@users.noreply.github.com>
2026-07-20 14:59:18 -0400

    Sincroniza a landing com 1.7.20.3

--- br/index.php
+++ br/index.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-const BR_LANDING_VERSION_FALLBACK = "1.7.20.2";
+const BR_LANDING_VERSION_FALLBACK = "1.7.20.3";
 require __DIR__ . "/runtime-core.php";
 require __DIR__ . "/runtime-telemetry.php";
 require __DIR__ . "/runtime-data.php";
