1c1df994ca7b3239dd1cc42c5328d02276e9985c
Wesley Ferreira <48494254+drwesleyadv@users.noreply.github.com>
2026-08-14 15:52:32 -0400

    fix: restrict telemetry routes to developer context

--- app/Runtime/Routing/RouteRegistry.php
+++ app/Runtime/Routing/RouteRegistry.php
@@ -9,10 +9,9 @@ final class RouteRegistry
 {
     private const SPECS = [
         'home' => [\Prontoo\Runtime\Dashboards\DashboardsRuntimeOperations01::class, 'page_home', false, false, [], ['patients', 'financial']],
-        'status' => [\Prontoo\Runtime\AdminPages\AdminPagesRuntimeOperations03::class, 'page_status', true, false, ['GET'], []],
+        'status' => [\Prontoo\Runtime\AdminPages\AdminPagesRuntimeOperations09::class, 'page_admin_performance', false, false, [], []],
         'login' => [\Prontoo\Runtime\AuthOnboarding\AuthOnboardingRuntimeOperations03::class, 'page_login', true, false, ['*'], []],
-        'login_telemetry_wave' => [\Prontoo\Runtime\AuthOnboarding\AuthOnboardingRuntimeOperations07::class, 'page_login_telemetry_wave', true, true, ['*'], []],
-        'login_autotest' => [\Prontoo\Runtime\SupportFoundation\SupportFoundationRuntimeOperations02::class, 'page_login_autotest', true, false, ['*'], []],
+        'login_autotest' => [\Prontoo\Runtime\SupportFoundation\SupportFoundationRuntimeOperations02::class, 'page_login_autotest', true, false, ['GET'], []],
         'mfa' => [\Prontoo\Runtime\AuthOnboarding\AuthOnboardingRuntimeOperations02::class, 'page_mfa', true, false, ['*'], []],
         'mobile_web_access' => [\Prontoo\Runtime\PublicWeb\PublicWebRuntimeOperations01::class, 'page_mobile_web_access', true, false, ['GET'], []],
         'goal_status' => [\Prontoo\Runtime\Financial\FinancialRuntimeOperations01::class, 'page_goal_status', false, true, [], ['financial']],
@@ -144,7 +143,7 @@ final class RouteRegistry
                     $failures[] = 'light_method_invalid:' . $name . ':' . $method;
                 }
             }
-            if ($definition->json && $definition->public && $name !== 'login_telemetry_wave') {
+            if ($definition->json && $definition->public) {
                 $failures[] = 'unexpected_public_json:' . $name;
             }
         }
